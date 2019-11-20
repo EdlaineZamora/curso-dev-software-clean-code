acb41221addb53b2f4f91873af79d7b7a84df1b1
Edlaine Zamora <ezamora@thoughtworks.com>
2019-11-19 21:16:43 -0300

    Add first exercise

---
+++ src/main/java/br/com/tw/cursodevsoftwarecleancode/Main.java
@@ -0,0 +1,34 @@
+package br.com.tw.cursodevsoftwarecleancode;
+
+import br.com.tw.cursodevsoftwarecleancode.exercise1.Project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Project project = new Project();
+        //Seta Dias restantes para o fim do projeto
+        project.setDiresfipro(80);
+        project.setEntregue(false);
+
+        String result = checkProject(project);
+
+        //Print result
+        System.out.println("Resultado do projeto: " + result);
+    }
+
+    private static String checkProject(Project project) {
+        //Dias restantes para o fim do projeto
+        int diresfipro = project.getDiresfipro();
+
+        if (diresfipro == -1) {
+            return "Não falta nenhum dia para o fim do projeto";
+        }
+
+        if (diresfipro < 90 && !project.isEntregue()) {
+            return "O projeto está apertado.";
+        } else {
+            return null;
+        }
+    }
+
+}
---
+++ src/main/java/br/com/tw/cursodevsoftwarecleancode/exercise1/Project.java
@@ -0,0 +1,27 @@
+package br.com.tw.cursodevsoftwarecleancode.exercise1;
+
+public class Project {
+    private int diresfipro;
+    private boolean entregue;
+
+    public int getDiresfipro() {
+        if (this.diresfipro < 0) {
+            //Código de erro
+            return -1;
+        } else {
+            return this.diresfipro;
+        }
+    }
+
+    public boolean isEntregue() {
+        return this.entregue;
+    }
+
+    public void setDiresfipro(int diresfipro) {
+        this.diresfipro = diresfipro;
+    }
+
+    public void setEntregue(boolean entregue) {
+        this.entregue = entregue;
+    }
+}
