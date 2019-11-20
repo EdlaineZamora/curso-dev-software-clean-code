f488bd5ed83e63b2ab963f77319acd251a22f84c
Edlaine Zamora <ezamora@thoughtworks.com>
2019-11-19 22:13:39 -0300

    Exercise 1

--- src/main/java/br/com/tw/cursodevsoftwarecleancode/exercise1/Main.java
+++ src/main/java/br/com/tw/cursodevsoftwarecleancode/exercise1/Main.java
@@ -7,9 +7,7 @@ public class Main {
         //Seta Dias restantes para o fim do projeto
         project.setDiresfipro(80);
         project.setEntregue(false);
-
         String result = checkProject(project);
-
         //Print result
         System.out.println("Resultado do projeto: " + result);
     }
@@ -17,11 +15,9 @@ public class Main {
     private static String checkProject(Project project) {
         //Dias restantes para o fim do projeto
         int diresfipro = project.getDiresfipro();
-
         if (diresfipro == -1) {
             return "Não falta nenhum dia para o fim do projeto";
         }
-
         if (diresfipro < 90 && !project.isEntregue()) {
             return "O projeto está apertado.";
         } else {
